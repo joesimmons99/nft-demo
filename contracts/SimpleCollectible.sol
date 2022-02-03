f8fbe5632ef471ed8ef98a1e2884c9a3064ec207
“Joe <joe_simmons_99@yahoo.com>
2022-02-02 22:22:25 -0800

    added image and compiled SimpleCollectible.sol

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity 0.6.0;
-
+pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+contract SimpleCollectible is ERC721 {
+    uint256 public tokenCounter;
+
+    constructor () public ERC721 ("Dogie", "DOG") {
+        tokenCounter = 0; // Initialize token counter to 0
+    }
+
+    function createCollectible() public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
+}
