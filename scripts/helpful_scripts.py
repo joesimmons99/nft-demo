716c7486192e23825766222a2c2a1f1dd3fc104f
“Joe <joe_simmons_99@yahoo.com>
2022-02-09 09:01:57 -0800

    added and tested advanced_collectible/deploy_and_create.py

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,4 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, Contract, LinkToken, VRFCoordinatorMock
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhar", "development", "ganache", "mainnet-fork"]
 OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
@@ -11,3 +11,45 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+contract_to_mock = {"link_token": LinkToken, "vrf_coordinator": VRFCoordinatorMock}
+
+def get_contract(contract_name):
+    """If you want to use this function, go to the brownie config and add a new entry for
+    the contract that you want to be able to 'get'. Then add an entry in the variable 'contract_to_mock'.
+    You'll see examples like the 'link_token'.
+        This script will then either:
+            - Get a address from the config
+            - Or deploy a mock to use for a network that doesn't have it
+        Args:
+            contract_name (string): This is the name that is referred to in the
+            brownie config and 'contract_to_mock' variable.
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            Contract of the type specificed by the dictionary. This could be either
+            a mock or the 'real' contract on a live network.
+    """
+    contract_type = contract_to_mock[contract_name]    
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract        
+
+def deploy_mocks():
+    """
+    Use this script if you want to deploy mocks to testnet
+    """    
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks...")
+    account = get_account()
+    print("Deploying mock link_token...")
+    link_token = LinkToken.deploy({"from": account})
+    print(f"Link token deployed to {link_token.address}")
+    print("Deploying mock VRF Coordinator...")
+    vrf_coordinator = VRFCoordinatorMock.deploy(link_token.address, {"from": account})
