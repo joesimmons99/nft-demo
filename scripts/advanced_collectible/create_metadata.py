05bd512398a6282eda1e0baae20e9610c932e672
“Joe <joe_simmons_99@yahoo.com>
2022-02-11 17:08:35 -0800

    11hr 16min. Mid-process of trying to install ipfs

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -1,6 +1,7 @@
 from brownie import AdvancedCollectible, network
 from scripts.helpful_scripts import get_breed
 from metadata.sample_metadata import metadata_template
+from pathlib import Path
 
 def main():
     advanced_collectible = AdvancedCollectible[-1]
@@ -11,4 +12,12 @@ def main():
         metadata_file_name = (
             f".metadata/{network.show_active()}/{token_id}-{breed}.json"
         )
+        collectible_metadata = metadata_template
         print(metadata_file_name)
+        if Path(metadata_file_name).exists():
+            print(f"Metadata file {metadata_file_name} already exists! Delete it to overwrite it.")
+        else:
+            print(f"Creating metadata file {metadata_file_name}")
+            collectible_metadata["name"] = breed
+            collectible_metadata["description"] = f"An adorable {breed} pup!"
+            print(collectible_metadata)
